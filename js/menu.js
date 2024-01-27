0d2da37a0dc6f74cad33ade5f68552caab7aa7a8
Alejandro Quiceno Pérez <alejo.perez2905@gmail.com>
2024-01-26 22:30:36 -0500

    se corrige los errores de archivos

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const btnOpen = document.querySelector("#open-menu");
 const btnClose = document.querySelector("#close-menu");
 const nav = document.querySelector("#menu");
@@ -12,17 +11,3 @@ btnClose.addEventListener("click", (e) => {
     e.preventDefault();
     nav.classList.remove("visible");
 })
-=======
-// // selector
-// var menu = document.querySelector('.hamburger');
-
-// // method
-// function toggleMenu(event) {
-//     this.classList.toggle('is-active');
-//     document.querySelector(".menuppal").classList.toggle("is_active");
-//     event.preventDefault();
-// }
-
-// // event
-// menu.addEventListener('click', toggleMenu, false);
->>>>>>> a142f64f78544b28996feb0f5a947273eef34fb2
