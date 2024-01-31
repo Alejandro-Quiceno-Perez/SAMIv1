0c2cfa45a2e9ce2c265667815cd14c9f2763e70e
Alejandro Quiceno Pérez <alejo.perez2905@gmail.com>
2024-01-30 20:15:17 -0500

    Se actualiza el slider

--- js/slider.js
+++ js/slider.js
@@ -1,4 +1,20 @@
+const containerImg = document.querySelector("#containerImg");
+const listImg = ["./img/amb1.jpg", "./img/amb2.jpg", "./img/amb3.jpg", "./img/amb4.jpg","./img/amb5.jpg","./img/amb6.jpg"];
 
 
+setInterval(() => {
+    printImg(nRandom(listImg.length))
+}, 6000);
 
+function nRandom(max) {
+    const n = Math.floor(Math.random() * max);
+    return parseInt(n);
+}
+
+
+function printImg(n) {
+    containerImg.innerHTML = `
+        <img src="${listImg[n]}" class="" alt="">
+    `;
+}
 
