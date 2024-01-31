0a744b2a7f119f2650db255ac0bba4c65255972a
Alejandro Quiceno Pérez <alejo.perez2905@gmail.com>
2024-01-30 20:19:26 -0500

    Actualizacion

--- js/slider.js
+++ js/slider.js
@@ -1,7 +1,6 @@
 const containerImg = document.querySelector("#containerImg");
 const listImg = ["./img/amb1.jpg", "./img/amb2.jpg", "./img/amb3.jpg", "./img/amb4.jpg","./img/amb5.jpg","./img/amb6.jpg"];
 
-
 setInterval(() => {
     printImg(nRandom(listImg.length))
 }, 6000);
@@ -10,7 +9,6 @@ function nRandom(max) {
     const n = Math.floor(Math.random() * max);
     return parseInt(n);
 }
-
 
 function printImg(n) {
     containerImg.innerHTML = `
