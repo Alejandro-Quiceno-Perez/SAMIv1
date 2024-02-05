70ace307ee99984afeeacfc4d9db04ff40ef588d
Val <anav.usuga@gmail.com>
2024-02-05 11:46:59 -0500

    updates

--- js/login.js
+++ js/login.js
@@ -1,27 +1,95 @@
-// LOGIN GOOGLE
-var googleUser = {};
-var startApp = function() {
-  gapi.load('auth2', function(){
-    // Retrieve the singleton for the GoogleAuth library and set up the client.
-    auth2 = gapi.auth2.init({
-      client_id: '894515735816-eeutj89dhsltkalp9k79d1n8vvv9ud6o.apps.googleusercontent.com',
-      cookiepolicy: 'single_host_origin',
-      // Request scopes in addition to 'profile' and 'email'
-      //scope: 'additional_scope'
-    });
-    attachSignin(document.getElementById('customBtn'));
-  });
-};
+// Login
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const confirmation = document.getElementById("confirmation");
+const btnLogin = document.getElementById("login");
 
-function attachSignin(element) {
-  console.log(element.id);
-  auth2.attachClickHandler(element, {},
-      function(googleUser) {
-        document.getElementById('name').innerText = "Signed in: " +
-            googleUser.getBasicProfile().getName();
-      }, function(error) {
-        alert(JSON.stringify(error, undefined, 2));
-      });
+// Register
+const emailRegister = document.getElementById("adressEmail")
+const passwordRegister = document.getElementById("passwordRegister");
+const passwordRegisterConfirmation = document.getElementById("passwordConfirmation");
+const btnRegister = document.getElementById("register");
+
+const urlDesplegada = "https://sami-i7mr.onrender.com";
+const urlLocal = "http://localhost:3000";
+
+btnRegister.addEventListener("click", e => {
+    e.preventDefault();
+    createUser();
+});
+
+console.log(btnRegister)
+
+
+// Create user 
+async function createUser() {
+    try {
+
+        const userExist = await fetch(`${urlDesplegada}/users?email=${emailRegister.value}`)
+        const data = await userExist.json();
+
+
+        if (data.length > 1) {
+            console.log("usuario YA registrado ");
+            return;
+        }
+
+
+        const user = {
+            email: emailRegister.value,
+            password: passwordRegister.value,
+            role: "user"
+        }
+
+
+        if (passwordRegister.value === passwordRegisterConfirmation.value) {
+            const createUser = await fetch(`${urlDesplegada}/users`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body:JSON.stringify(user)
+            });
+
+            console.log(createUser);
+
+            return;
+        }
+        console.log("Las contraseñas NO coinciden");
+
+    } catch (error) {
+        console.log(error.message);
+    }
 }
 
-startApp();
+// Login
+
+btnLogin.addEventListener("click", e => {
+    e.preventDefault();
+    validateLogin();
+    console.log(btnLogin)
+})
+
+async function validateLogin() {
+    try {
+        const login = await fetch(`${urlDesplegada}/users?email=${email.value}`);
+        const data = await login.json();
+        
+        if(data.length < 1) {
+            console.log('user no creado')
+        }
+        
+        if(data[0].email === "samiAdmin@sami.com") {
+            window.open("../../admin.html", "_blank");
+        }
+        
+        if(data[0].email !== "samiAdmin@sami.com") {
+            console.log("Entro");
+            window.location.href = "../../user-page.html";
+        }
+        localStorage.setItem("userSami", data.email);
+        console.log(data);
+    } catch (error) {
+        console.log(error.message);
+    }
+}
