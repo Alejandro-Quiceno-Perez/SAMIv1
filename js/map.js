f44bcc609394987c5941f7bd79e03591d65a3cdd
juansefdz <jsfm81091@gmail.com>
2024-02-07 15:49:56 -0500

    UPDATE

--- js/map.js
+++ js/map.js
@@ -1,144 +1,54 @@
-//Selectors
-// const direction = document.getElementById("direction");
+let map;
+let directionsService;
+let directionsRenderer;
 
-//token
-const accessToken =
-  "pk.eyJ1Ijoia3dtZWppYSIsImEiOiJjbGl2eWk4eWwxb3dhM3Bxdm5kNGtpOXRrIn0.RaBQJtXzaW3dBHodhcQg2Q";
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+      center: { lat: 6.2193724219356685, lng: -75.58339605807475 },  //se centra en de moda outlet
+        zoom: 12,
+    });
 
-const country = "CO";
+    directionsService = new google.maps.DirectionsService();
+    directionsRenderer = new google.maps.DirectionsRenderer();
+    directionsRenderer.setMap(map);
 
-if (!"geolocation" in navigator) {
-  console.error("Geolocalización no disponible");
-} else {
-  // if (direction.value.trim() !== "") {
-  //   obtenerCoordenadas(direction.value);
-  // }
-  navigator.geolocation.getCurrentPosition(
-    (position) => {
-      console.log(position);
-      mapboxgl.accessToken = accessToken;
-      let map = new mapboxgl.Map({
-        container: "map",
-        style: "mapbox://styles/mapbox/outdoors-v12",
-        center: [position.coords.longitude, position.coords.latitude],
-        zoom: 10,
-      });
+    const geocoder = new google.maps.Geocoder();
 
-      let markers = [];
-      let routeLayerID = "route";
-      let initialMarker = new mapboxgl.Marker()
-        .setLngLat([position.coords.longitude, position.coords.latitude])
-        .addTo(map);
-      markers.push(initialMarker);
-      // }
+    let emergencyPoint; // Punto A seleccionado por el usuario, lugar de emergencia
+    let ambulancePoint = { lat: 0, lng: 0 }; // Punto B se captura por medio de json ubiaciones de hospitales
 
-      function updateRoute() {
-        if (markers.length === 2) {
-          let start = markers[0].getLngLat();
-          let end = markers[1].getLngLat();
-          let directionsRequest =
-            "https://api.mapbox.com/directions/v5/mapbox/driving/" +
-            start.lng +
-            "," +
-            start.lat +
-            ";" +
-            end.lng +
-            "," +
-            end.lat +
-            "?geometries=geojson&access_token=" +
-            mapboxgl.accessToken;
-
-          fetch(directionsRequest)
-            .then((response) => response.json())
-            .then((data) => {
-              if (data.routes.length > 0) {
-                let route = data.routes[0].geometry;
-                if (map.getSource(routeLayerID)) {
-                  map.getSource(routeLayerID).setData(route);
-                } else {
-                  map.addLayer({
-                    id: routeLayerID,
-                    type: "line",
-                    source: {
-                      type: "geojson",
-                      data: route,
-                    },
-                    layout: {},
-                    paint: {
-                      "line-width": 2,
-                      "line-color": "#1f00bd",
-                    },
-                  });
-                }
-              }
-            })
-            .catch((error) =>
-              console.error("Error en la solicitud de la ruta:", error)
-            );
-        }
-      }
-
-      map.on("click", function (e) {
-        let newMarker = new mapboxgl.Marker().setLngLat(e.lngLat).addTo(map);
-        markers.push(newMarker);
-
-        if (markers.length > 2) {
-          markers[0].remove();
-          markers.shift();
-        }
-
-        updateRoute();
-      });
-    },
-    (error) => {
-      console.error(error);
-    }
-  );
-}
-
-async function obtenerCoordenadas(direccion) {
-  try {
-    const apiUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${direccion}.json?country=${country}&access_token=${accessToken}`;
-    fetch(apiUrl)
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.features && data.features.length > 0) {
-          // const coordenadas = data.features[0].geometry.coordinates;
-          mapboxgl.accessToken = accessToken;
-          let map = new mapboxgl.Map({
-            container: "map",
-            style: "mapbox://styles/mapbox/navigation-day-v1",
-            center: [
-              data.features[0].geometry.coordinates,
-              position.coords.latitude,
-            ],
-            zoom: 10,
-          });
-
-          let markers = [];
-          // let routeLayerID = "route";
-          let initialMarker = new mapboxgl.Marker()
-            .setLngLat([data.features[0].center[0], data.features[0].center[1]])
-            .addTo(map);
-          markers.push(initialMarker);
-
-          console.log(data);
+    // Evento al hacer clic en el mapa
+    map.addListener("click", (event) => {
+        if (!emergencyPoint) {
+            emergencyPoint = event.latLng;
+            placeMarker(emergencyPoint);
+            geocodeLatLng(geocoder, emergencyPoint);
         } else {
-          console.log(`No se encontraron coordenadas para "${direccion}"`);
+            alert("Ya has seleccionado tu lugar de emergencia!");
         }
-      });
-  } catch (error) {
-    console.error("Error al realizar la solicitud:", error);
-  }
+    });
+
+    //calcula y muestra la ruta entre la emergencia y el punto designado por la ambulancia
+    calculateAndDisplayRoute(emergencyPoint, ambulancePoint);
 }
 
-// function buscarCoordenadas() {
-//   const direccionInput = document.getElementById("direccionInput");
-//   const direccion = direccionInput.value;
+function placeMarker(location) {
+    const marker = new google.maps.Marker({
+        position: location,
+        map: map,
+    });
+}
 
-//   if (direccion.trim() !== "") {
-//     obtenerCoordenadas(direccion);
-//   } else {
-//     console.log("Por favor, ingrese una dirección válida.");
-//   }
-// }
+function geocodeLatLng(geocoder, latLng) {
+    geocoder.geocode({ location: latLng }, (results, status) => {
+        if (status === "OK") {
+            if (results[0]) {
+                console.log(results[0].formatted_address); //muestra la dir seleccionada
+            } else {
+                console.log("No se encontraron resultados");
+            }
+        } else {
+            console.log("Error al geocodificar: " + status);
+        }
+    });
+}
--- js/slider.js
+++ js/slider.js
@@ -6,96 +6,92 @@ value.textContent = slider.value;
 
 slider.oninput = function () {
   value.textContent = this.value;
-// Add your custom JavaScript here
-$(document).ready(function () {
+
+  $(document).ready(function () {
     $("#custom-slider").on("input change", function () {
+      let value = parseFloat($(this).val()).toFixed(1);
+      $("#slider-value").text("Grado: " + value);
 
-        let value = parseFloat($(this).val()).toFixed(1);
-        $('#slider-value').text('Grado: ' + value);
-
-  // Calculate color based on the slider value
-  if (this.value <= 20) {
-    boxSlider.className = "box-slider color-1";
-  } else if (this.value <= 40) {
-    boxSlider.className = "box-slider color-2";
-  } else if (this.value <= 60) {
-    boxSlider.className = "box-slider color-3";
-  } else if (this.value <= 80) {
-    boxSlider.className = "box-slider color-4";
-  } else {
-    boxSlider.className = "box-slider color-5";
-  }
-});
-        // Calculate color based on the slider value
-        let colorClass;
-        if (value <= 1) {
-            colorClass = 'color-0';
-        } else if (value <= 2) {
-            colorClass = 'color-1';
-        } else if (value <= 3) {
-            colorClass = 'color-2';
-        } else if (value <= 4) {
-            colorClass = 'color-3';
-        } else if (value <= 5) {
-            colorClass = 'color-4';
-        } else {
-            colorClass = 'color-5';
-        }
-
-        $('#slider-value').attr('class', 'color-' + Math.round(value));
+      // Calculate color based on the slider value
+      if (this.value <= 20) {
+        boxSlider.className = "box-slider color-1";
+      } else if (this.value <= 40) {
+        boxSlider.className = "box-slider color-2";
+      } else if (this.value <= 60) {
+        boxSlider.className = "box-slider color-3";
+      } else if (this.value <= 80) {
+        boxSlider.className = "box-slider color-4";
+      } else {
+        boxSlider.className = "box-slider color-5";
+      }
     });
-}
 
-document.addEventListener("DOMContentLoaded", function () {
-    let sliderContainer = document.getElementById("imageSlider");
-    let slides = sliderContainer.querySelectorAll(".slide");
-
-    let currentSlideIndex = 0;
-    setInterval(changeSlide, 8000);
-
-    function changeSlide() {
-        slides[currentSlideIndex].style.opacity = 0;
-
-        currentSlideIndex = (currentSlideIndex + 1) % slides.length;
-
-        fadeIn(slides[currentSlideIndex]);
-    }
-
-    function fadeIn(element) {
-        let opacity = 0;
-        let interval = setInterval(function () {
-            if (opacity < 1) {
-                opacity += 2;
-                element.style.opacity = opacity;
-            } else {
-                clearInterval(interval);
-            }
-        }, 50);
-    }
-});// Add your custom JavaScript here
-$(document).ready(function () {
-  $("#custom-slider").on("input change", function () {
-   
-    let value = parseFloat($(this).val()).toFixed(1);
-    $('#slider-value').text('Grado: ' + value);
-
-    // Calculate color based on the slider value
     let colorClass;
     if (value <= 1) {
-      colorClass = 'color-0';
+      colorClass = "color-0";
     } else if (value <= 2) {
-      colorClass = 'color-1';
+      colorClass = "color-1";
     } else if (value <= 3) {
-      colorClass = 'color-2';
+      colorClass = "color-2";
     } else if (value <= 4) {
-      colorClass = 'color-3';
+      colorClass = "color-3";
     } else if (value <= 5) {
-      colorClass = 'color-4';
+      colorClass = "color-4";
     } else {
-      colorClass = 'color-5';
+      colorClass = "color-5";
     }
 
-    $('#slider-value').attr('class', 'color-' + Math.round(value));
+    $("#slider-value").attr("class", "color-" + Math.round(value));
+  });
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  let sliderContainer = document.getElementById("imageSlider");
+  let slides = sliderContainer.querySelectorAll(".slide");
+
+  let currentSlideIndex = 0;
+  setInterval(changeSlide, 8000);
+
+  function changeSlide() {
+    slides[currentSlideIndex].style.opacity = 0;
+
+    currentSlideIndex = (currentSlideIndex + 1) % slides.length;
+
+    fadeIn(slides[currentSlideIndex]);
+  }
+
+  function fadeIn(element) {
+    let opacity = 0;
+    let interval = setInterval(function () {
+      if (opacity < 1) {
+        opacity += 2;
+        element.style.opacity = opacity;
+      } else {
+        clearInterval(interval);
+      }
+    }, 50);
+  }
+});
+$(document).ready(function () {
+  $("#custom-slider").on("input change", function () {
+    let value = parseFloat($(this).val()).toFixed(1);
+    $("#slider-value").text("Grado: " + value);
+
+    let colorClass;
+    if (value <= 1) {
+      colorClass = "color-0";
+    } else if (value <= 2) {
+      colorClass = "color-1";
+    } else if (value <= 3) {
+      colorClass = "color-2";
+    } else if (value <= 4) {
+      colorClass = "color-3";
+    } else if (value <= 5) {
+      colorClass = "color-4";
+    } else {
+      colorClass = "color-5";
+    }
+
+    $("#slider-value").attr("class", "color-" + Math.round(value));
   });
 });
-
